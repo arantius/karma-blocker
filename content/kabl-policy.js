3e1b7649f2195e26e33a3e2397ebb35a80e08507
Anthony Lieuallen <arantius@gmail.com>
2007-02-04 18:41:51 +0000

    reorg constant declaration, remove nearly empty "kabl.js"

--- content/kabl-policy.js
+++ content/kabl-policy.js
@@ -199,8 +199,11 @@ var gKablFactory={
 // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ //
 
 // Initialization and registration
-if ('undefined'==typeof(Components.classes[KABL_CONTRACTID])) {
+if ('undefined'==typeof(Components.classes[_KABL_CONTRACTID])) {
 	(function() { // to keep from munging with scope
+		const CONTENTPOLICY_CONTRACTID="@mozilla.org/layout/content-policy;1";
+		const CONTENTPOLICY_DESCRIPTION="Content policy service";
+
 		// Component registration
 		var compMgr=Components.manager
 			.QueryInterface(Components.interfaces.nsIComponentRegistrar);
@@ -210,7 +213,7 @@ if ('undefined'==typeof(Components.classes[KABL_CONTRACTID])) {
 			cid, CONTENTPOLICY_DESCRIPTION, CONTENTPOLICY_CONTRACTID, gKablFactory
 		);
 		compMgr.registerFactory(
-			KABL_CID, CONTENTPOLICY_DESCRIPTION, KABL_CONTRACTID, gKablFactory
+			_KABL_CID, CONTENTPOLICY_DESCRIPTION, _KABL_CONTRACTID, gKablFactory
 		);
 	})();
 }
--- content/kabl.js
+++
@@ -1,5 +0,0 @@
-const KABL_CONTRACTID="@arantius.com/kabl;1";
-const KABL_CID=Components.ID('{cabe6b3f-578c-480f-a2f0-68bc4b7a1142}');
-
-const CONTENTPOLICY_CONTRACTID="@mozilla.org/layout/content-policy;1";
-const CONTENTPOLICY_DESCRIPTION="Content policy service";
