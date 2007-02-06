322038e196e7cb22d116b1877f8a9c202853e9ef
Anthony Lieuallen <arantius@gmail.com>
2007-02-06 02:33:35 +0000

    license noticies

--- components/kabl-comp-policy.js
+++ components/kabl-comp-policy.js
@@ -1,3 +1,38 @@
+// ***** BEGIN LICENSE BLOCK *****
+// Version: MPL 1.1/GPL 2.0/LGPL 2.1
+//
+// The contents of this file are subject to the Mozilla Public License Version
+// 1.1 (the 'License'); you may not use this file except in compliance with
+// the License. You may obtain a copy of the License at
+// http://www.mozilla.org/MPL/
+//
+// Software distributed under the License is distributed on an 'AS IS' basis,
+// WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+// for the specific language governing rights and limitations under the
+// License.
+//
+// The Initial Developer of the Original Code is Joe Hewitt.
+//
+// Portions created by the Initial Developer are Copyright (C) 2005
+// the Initial Developer. All Rights Reserved.
+//
+// Contributor(s):
+//   Anthony Lieuallen, Copyright (C) 2007
+//
+// Alternatively, the contents of this file may be used under the terms of
+// either the GNU General Public License Version 2 or later (the 'GPL'), or
+// the GNU Lesser General Public License Version 2.1 or later (the 'LGPL'),
+// in which case the provisions of the GPL or the LGPL are applicable instead
+// of those above. If you wish to allow use of your version of this file only
+// under the terms of either the GPL or the LGPL, and not to allow others to
+// use your version of this file under the terms of the MPL, indicate your
+// decision by deleting the provisions above and replace them with the notice
+// and other provisions required by the GPL or the LGPL. If you do not delete
+// the provisions above, a recipient may use your version of this file under
+// the terms of any one of the MPL, the GPL or the LGPL.
+//
+// ***** END LICENSE BLOCK *****
+
 const _KABL_CONTRACTID="@arantius.com/kabl;1";
 const _KABL_CID=Components.ID('{cabe6b3f-578c-480f-a2f0-68bc4b7a1142}');
 
@@ -50,7 +85,6 @@ var gKablModule={
 			var loader=Components.classes[JSLOADER_CONTRACTID]
 				.getService(Components.interfaces.mozIJSSubScriptLoader);
 
-			loader.loadSubScript('chrome://kabl/content/kabl.js');
 			loader.loadSubScript('chrome://kabl/content/kabl-pref.js');
 			loader.loadSubScript('chrome://kabl/content/kabl-prefobs.js');
 			loader.loadSubScript('chrome://kabl/content/kabl-policy.js');
--- content/kabl-config.js
+++ content/kabl-config.js
@@ -1,23 +1,35 @@
-// from adblock plus
-function gKablLoadInBrowser(url) {
-	var windowMediator=Components
-		.classes["@mozilla.org/appshell/window-mediator;1"]
-		.getService(Components.interfaces.nsIWindowMediator);
-	var currentWindow=windowMediator.getMostRecentWindow("navigator:browser");
-	if (currentWindow) {
-		try {
-			currentWindow.delayedOpenTab(url);
-		}
-		catch(e) {
-			currentWindow.loadURI(url);
-		}
-	} else {
-		var protocolService=Components
-			.classes['@mozilla.org/uriloader/external-protocol-service;1']
-			.getService(Components.interfaces.nsIExternalProtocolService);
-		protocolService.loadUrl(url);
-	}
-}
+// ***** BEGIN LICENSE BLOCK *****
+// Version: MPL 1.1/GPL 2.0/LGPL 2.1
+//
+// The contents of this file are subject to the Mozilla Public License Version
+// 1.1 (the 'License'); you may not use this file except in compliance with
+// the License. You may obtain a copy of the License at
+// http://www.mozilla.org/MPL/
+//
+// Software distributed under the License is distributed on an 'AS IS' basis,
+// WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+// for the specific language governing rights and limitations under the
+// License.
+//
+// The Initial Developer of the Original Code is Anthony Lieuallen.
+//
+// Portions created by the Initial Developer are Copyright (C) 2005
+// the Initial Developer. All Rights Reserved.
+//
+//
+// Alternatively, the contents of this file may be used under the terms of
+// either the GNU General Public License Version 2 or later (the 'GPL'), or
+// the GNU Lesser General Public License Version 2.1 or later (the 'LGPL'),
+// in which case the provisions of the GPL or the LGPL are applicable instead
+// of those above. If you wish to allow use of your version of this file only
+// under the terms of either the GPL or the LGPL, and not to allow others to
+// use your version of this file under the terms of the MPL, indicate your
+// decision by deleting the provisions above and replace them with the notice
+// and other provisions required by the GPL or the LGPL. If you do not delete
+// the provisions above, a recipient may use your version of this file under
+// the terms of any one of the MPL, the GPL or the LGPL.
+//
+// ***** END LICENSE BLOCK *****
 
 function gKablConfigOpen() {
 	document.getElementById('enabled').setAttribute('checked', gKablEnabled);
@@ -78,5 +90,26 @@ function gKablSetStatusLabel(type, msg) {
 		errmsg.setAttribute('hidden', false);
 	} else {
 		errmsg.setAttribute('hidden', true);
+	}
+}
+
+// This function originates from AdBlock Plus, reused under MPL.
+function gKablLoadInBrowser(url) {
+	var windowMediator=Components
+		.classes["@mozilla.org/appshell/window-mediator;1"]
+		.getService(Components.interfaces.nsIWindowMediator);
+	var currentWindow=windowMediator.getMostRecentWindow("navigator:browser");
+	if (currentWindow) {
+		try {
+			currentWindow.delayedOpenTab(url);
+		}
+		catch(e) {
+			currentWindow.loadURI(url);
+		}
+	} else {
+		var protocolService=Components
+			.classes['@mozilla.org/uriloader/external-protocol-service;1']
+			.getService(Components.interfaces.nsIExternalProtocolService);
+		protocolService.loadUrl(url);
 	}
 }
