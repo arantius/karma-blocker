8e983f3fffcc7623459ad3873528155217e669c8
Anthony Lieuallen <arantius@gmail.com>
2011-03-06 22:15:57 +0000

    Refactor (exposed) members of gKablPolicy to private module globals.

--- content/kabl-policy.js
+++ content/kabl-policy.js
@@ -40,19 +40,201 @@ Cu.import('chrome://kabl/content/kabl-parse.js');
 Cu.import('chrome://kabl/content/kabl-pref.js');
 Cu.import("resource://gre/modules/XPCOMUtils.jsm");
 
+const gKablCollapseMarker=String(Math.floor(Math.random()*100000));
+const UNORDERED_NODE_SNAPSHOT_TYPE=6;
+const COLLAPSE_TEXT_LENGTH=25;
+
 //\\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ //
 
+// constructor for details of the request
+function Fields(type, loc, org, node) {
+	this['$type']=type;
+
+	this['$url']=loc.spec;
+	this['$url.host']=loc.host;
+	this['$url.path']=loc.path;
+	this['$url.scheme']=loc.scheme;
+
+	// Set base values.
+	this['$thirdParty']=false;
+	this['$origin']=undefined;
+	this['$origin.host']=undefined;
+	this['$origin.path']=undefined;
+	this['$origin.scheme']=undefined;
+	this['$origin.tag']=undefined;
+
+	// Conditionally override them based on scheme.
+	switch (org.scheme) {
+	case 'about':
+		this['$origin']=org.spec;
+		setOriginTag(this, node);
+
+		break;
+	case 'file':
+	case 'chrome':
+		this['$origin']=org.spec;
+		this['$origin.path']=org.path;
+		this['$origin.scheme']=org.scheme;
+
+		setOriginTag(this, node);
+
+		break;
+	case 'ftp':
+	case 'http':
+	case 'https':
+		var lHost=loc.host;
+		var oHost=org.host;
+		if (!lHost.match(/^[0-9.]+$/)) {
+			lHost=hostToTld(lHost);
+			oHost=hostToTld(oHost);
+		}
+		this['$thirdParty']=(lHost!=oHost);
+
+		this['$origin']=org.spec;
+		this['$origin.host']=org.host;
+		this['$origin.path']=org.path;
+		this['$origin.scheme']=org.scheme;
+
+		setOriginTag(this, node);
+
+		break;
+	default:
+		if (gKablPrefs.debug) {
+			dump(
+				'kabl error condition, unknown origin scheme for\n    '+
+				org.spec+'\n'
+			);
+		}
+		break;
+	}
+
+	for (var i=0, key=null; key=fieldNames[i]; i++) {
+		if ('string'==typeof this[key]) this[key]=this[key].toLowerCase();
+	}
+
+	this.node=node;
+}
+
 function cloneObject(obj) {
-		if (null==obj || 'object'!=typeof obj) {
-				return obj;
+	if (null==obj || 'object'!=typeof obj) {
+		return obj;
 	}
 
-		var out=new obj.constructor();
-		for (var key in obj) {
-				out[key]=cloneObject(obj[key]);
+	var out=new obj.constructor();
+	for (var key in obj) {
+		out[key]=cloneObject(obj[key]);
 	}
 
-		return out;
+	return out;
+}
+
+// true if group matches
+function evalGroup(group, fields) {
+	var flag;
+
+	for (var j=0, rule=null; rule=group.rules[j]; j++) {
+		flag=false;
+
+		switch (rule.op) {
+			case '==':
+				flag=fields[rule.field]==rule.val;
+				break;
+			case '!=':
+				flag=fields[rule.field]!=rule.val;
+				break;
+			case '=~':
+				flag=(new RegExp(rule.val)).test(fields[rule.field]);
+				break;
+			case '!~':
+				flag=!(new RegExp(rule.val)).test(fields[rule.field]);
+				break;
+			case '^=':
+				flag=fields[rule.field].substr(0, rule.val.length)==rule.val;
+				break;
+			case '$=':
+				flag=fields[rule.field]
+					.substr(fields[rule.field].length-rule.val.length)==rule.val;
+				break;
+		}
+
+		rule.match=flag;
+
+		if (flag && 'any'==group.match) {
+			return true;
+		} else if (!flag && 'all'==group.match) {
+			return false;
+		}
+	}
+
+	if (flag && 'all'==group.match) {
+		return true;
+	}
+}
+
+function hostToTld(host) {
+	// this terribly simple method seems to work well enough
+	return host.replace(/.*\.(.*......)/, '$1');
+}
+
+// evaluate if/how we should handle this type of score
+function evalScore(type, score, fields) {
+	if (('threshold'==type && score>=gKablRulesObj.threshold) ||
+		('cutoff'==type && score>=gKablRulesObj.cutoff)
+	) {
+		try {
+			var el=fields.node
+				.QueryInterface(Components.interfaces.nsIDOMNode);
+		} catch (e) {
+			if (gKablPrefs.debug) dump('Error in evalScore: '+e+'\n');
+			return;
+		}
+
+		// Attempt to hide the node, i.e. if a non-loaded image will
+		// result in an alt tag showing.
+		try {
+			el.setAttribute('style', 'display: none !important');
+		} catch (e) {
+			if (gKablPrefs.debug) dump('Error in evalScore: '+e+'\n');
+		}
+
+		// Mark the node for collapsing.
+		try {
+			if ('STYLE'!=el.tagName) {
+				el.setAttribute('kabl', gKablCollapseMarker);
+			}
+		} catch (e) {
+			if (gKablPrefs.debug) dump('Error in evalScore: '+e+'\n');
+		}
+
+		return REJECT;
+	} else if ('threshold'==type ||
+		('cutoff'==type && Math.abs(score)>=gKablRulesObj.cutoff)
+	) {
+		return ACCEPT;
+	} else {
+		return undefined;
+	}
+}
+
+function monitorAdd(fields, groups, score, flag) {
+	if (!monitorWin || monitorWin.closed) return;
+
+	var blocked=REJECT==flag;
+	monitorWin.gKablMonitor.add(fields, groups, score, blocked);
+}
+
+function setOriginTag(obj, node) {
+	obj['$origin.tag']=node.tagName;
+
+	if (!node.attributes) return;
+	for (var i=0, attr=null; attr=node.attributes.item(i); i++) {
+		obj['$origin.tag.'+attr.nodeName]=attr.nodeValue.toLowerCase();
+	}
+
+	if (obj['$origin.tag.width'] && obj['$origin.tag.height']) {
+		obj['$origin.tag.size']=obj['$origin.tag.width']
+			+'x'+obj['$origin.tag.height'];
+	}
 }
 
 function strippedTextContent(el) {
@@ -70,9 +252,29 @@ function strippedTextContent(el) {
 	return text;
 }
 
-const gKablCollapseMarker=String(Math.floor(Math.random()*100000));
-const UNORDERED_NODE_SNAPSHOT_TYPE=6;
-const COLLAPSE_TEXT_LENGTH=25;
+//Inherited from AdBlock Plus, utils.js
+function windowForNode(node) {
+	if (node && node.nodeType!=Components.interfaces.nsIDOMNode.DOCUMENT_NODE) {
+		node = node.ownerDocument;
+	}
+
+	if (!node || node.nodeType!=Components.interfaces.nsIDOMNode.DOCUMENT_NODE) {
+		return null;
+	}
+
+	return node.defaultView;
+}
+
+//\\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ //
+
+const ACCEPT=Components.interfaces.nsIContentPolicy.ACCEPT;
+const REJECT=Components.interfaces.nsIContentPolicy.REJECT_REQUEST;
+
+const fieldNames=['$type', '$thirdParty',
+    '$url', '$url.host', '$url.path', '$url.scheme',
+    '$origin', '$origin.host', '$origin.path', '$origin.scheme', '$origin.tag'];
+
+var monitorWin=null;
 
 //\\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ //
 
@@ -117,223 +319,21 @@ var gKablPolicy={
 			'content-policy', gKablPolicy.classDescription,
 			gKablPolicy.contractID, false, true);
 	},
-	ACCEPT:Components.interfaces.nsIContentPolicy.ACCEPT,
-	REJECT:Components.interfaces.nsIContentPolicy.REJECT_REQUEST,
 
-	monitorWin:null,
-
-	fieldNames:{
-		'$type':1, '$url':1, '$url.host':1, '$url.path':1, '$url.scheme':1,
-		'$thirdParty':1, '$origin':1, '$origin.host':1, '$origin.path':1,
-		'$origin.scheme':1, '$origin.tag':1
-	},
-
-	hostToTld:function(host) {
-		// this terribly simple method seems to work well enough
-		return host.replace(/.*\.(.*......)/, '$1');
-	},
-
-	// Inherited from AdBlock Plus, utils.js
-	windowForNode:function(node) {
-		if (node && node.nodeType!=Components.interfaces.nsIDOMNode.DOCUMENT_NODE) {
-			node = node.ownerDocument;
-		}
-
-		if (!node || node.nodeType!=Components.interfaces.nsIDOMNode.DOCUMENT_NODE) {
-			return null;
-		}
-
-		return node.defaultView;
-	},
-
-	// constructor for details of the request
-	Fields:function(type, loc, org, node) {
-		this['$type']=type;
-
-		this['$url']=loc.spec;
-		this['$url.host']=loc.host;
-		this['$url.path']=loc.path;
-		this['$url.scheme']=loc.scheme;
-
-		// Set base values.
-		this['$thirdParty']=false;
-		this['$origin']=undefined;
-		this['$origin.host']=undefined;
-		this['$origin.path']=undefined;
-		this['$origin.scheme']=undefined;
-		this['$origin.tag']=undefined;
-
-		// Conditionally override them based on scheme.
-		function setOriginTag(obj, node) {
-			obj['$origin.tag']=node.tagName;
-
-			if (!node.attributes) return;
-			for (var i=0, attr=null; attr=node.attributes.item(i); i++) {
-				obj['$origin.tag.'+attr.nodeName]=attr.nodeValue.toLowerCase();
-			}
-
-			if (obj['$origin.tag.width'] && obj['$origin.tag.height']) {
-				obj['$origin.tag.size']=obj['$origin.tag.width']
-					+'x'+obj['$origin.tag.height'];
-			}
-		}
-
-		switch (org.scheme) {
-		case 'about':
-			this['$origin']=org.spec;
-			setOriginTag(this, node);
-
-			break;
-		case 'file':
-		case 'chrome':
-			this['$origin']=org.spec;
-			this['$origin.path']=org.path;
-			this['$origin.scheme']=org.scheme;
-
-			setOriginTag(this, node);
-
-			break;
-		case 'ftp':
-		case 'http':
-		case 'https':
-			var lHost=loc.host;
-			var oHost=org.host;
-			if (!lHost.match(/^[0-9.]+$/)) {
-				lHost=gKablPolicy.hostToTld(lHost);
-				oHost=gKablPolicy.hostToTld(oHost);
-			}
-			this['$thirdParty']=(lHost!=oHost);
-
-			this['$origin']=org.spec;
-			this['$origin.host']=org.host;
-			this['$origin.path']=org.path;
-			this['$origin.scheme']=org.scheme;
-
-			setOriginTag(this, node);
-
-			break;
-		default:
-			if (gKablPrefs.debug) {
-				dump(
-					'kabl error condition, unknown origin scheme for\n    '+
-					org.spec+'\n'
-				);
-			}
-			break;
-		}
-
-		for (key in gKablPolicy.fieldNames) {
-			if ('string'==typeof this[key]) this[key]=this[key].toLowerCase();
-		}
-
-		this.node=node;
-	},
-
-	// true if group matches
-	evalGroup:function(group, fields) {
-		var flag;
-
-		for (var j=0, rule=null; rule=group.rules[j]; j++) {
-			flag=false;
-
-			switch (rule.op) {
-				case '==':
-					flag=fields[rule.field]==rule.val;
-					break;
-				case '!=':
-					flag=fields[rule.field]!=rule.val;
-					break;
-				case '=~':
-					flag=(new RegExp(rule.val)).test(fields[rule.field]);
-					break;
-				case '!~':
-					flag=!(new RegExp(rule.val)).test(fields[rule.field]);
-					break;
-				case '^=':
-					flag=fields[rule.field].substr(0, rule.val.length)==rule.val;
-					break;
-				case '$=':
-					flag=fields[rule.field]
-						.substr(fields[rule.field].length-rule.val.length)==rule.val;
-					break;
-			}
-
-			rule.match=flag;
-
-			if (flag && 'any'==group.match) {
-				return true;
-			} else if (!flag && 'all'==group.match) {
-				return false;
-			}
-		}
-
-		if (flag && 'all'==group.match) {
-			return true;
-		}
-	},
-
-	// evaluate if/how we should handle this type of score
-	evalScore:function(type, score, fields) {
-		if (('threshold'==type && score>=gKablRulesObj.threshold) ||
-			('cutoff'==type && score>=gKablRulesObj.cutoff)
-		) {
-			try {
-				var el=fields.node
-					.QueryInterface(Components.interfaces.nsIDOMNode);
-			} catch (e) {
-				if (gKablPrefs.debug) dump('Error in evalScore: '+e+'\n');
-				return;
-			}
-
-			// Attempt to hide the node, i.e. if a non-loaded image will
-			// result in an alt tag showing.
-			try {
-				el.setAttribute('style', 'display: none !important');
-			} catch (e) {
-				if (gKablPrefs.debug) dump('Error in evalScore: '+e+'\n');
-			}
-
-			// Mark the node for collapsing.
-			try {
-				if ('STYLE'!=el.tagName) {
-					el.setAttribute('kabl', gKablCollapseMarker);
-				}
-			} catch (e) {
-				if (gKablPrefs.debug) dump('Error in evalScore: '+e+'\n');
-			}
-
-			return this.REJECT;
-		} else if ('threshold'==type ||
-			('cutoff'==type && Math.abs(score)>=gKablRulesObj.cutoff)
-		) {
-			return this.ACCEPT;
-		} else {
-			return undefined;
-		}
-	},
-
-	// nsIKablPolicy
 	openMonitorWindow:function(parentWin) {
-		if (this.monitorWin && false===this.monitorWin.closed) {
-			return this.monitorWin;
+		if (monitorWin && false===monitorWin.closed) {
+			return monitorWin;
 		}
 
-		return this.monitorWin=parentWin.open(
+		return monitorWin=parentWin.open(
 			'chrome://kabl/content/kabl-monitor.xul', null,
 			'chrome,close=no,dependent,dialog,resizable'
 		);
 	},
 
 	closeMonitorWindow:function() {
-		this.monitorWin.close();
-		this.monitorWin=null;
-	},
-
-	monitorAdd:function(fields, groups, score, flag) {
-		if (!this.monitorWin || this.monitorWin.closed) return;
-
-		var blocked=this.REJECT==flag;
-		this.monitorWin.gKablMonitor.add(fields, groups, score, blocked);
+		monitorWin.close();
+		monitorWin=null;
 	},
 
 	collapse:function(event) {
@@ -381,14 +381,13 @@ var gKablPolicy={
 	// \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ //
 
 	// nsIContentPolicy
-	shouldLoad:function(
-		contentType, contentLocation, requestOrigin, requestingNode, mimeTypeGuess, extra
-	) {
+	shouldLoad:function(contentType, contentLocation, requestOrigin,
+			requestingNode, mimeTypeGuess, extra) {
 		try {
 
 		// when not enabled, let it through
 		if (!gKablPrefs.enabled) {
-			return this.ACCEPT;
+			return ACCEPT;
 		}
 
 		// it's not a remote scheme, definitely let it through
@@ -397,37 +396,37 @@ var gKablPolicy={
 			!contentLocation.schemeIs('chrome') &&
 			!contentLocation.schemeIs('ftp')
 		) {
-			return this.ACCEPT;
+			return ACCEPT;
 		}
 
 		// never block core parts of the browser
 		if ('chrome://global/'==contentLocation.spec.substr(0, 16)
 			|| 'chrome://browser/'==contentLocation.spec.substr(0, 17)
 		) {
-			return this.ACCEPT;
+			return ACCEPT;
 		}
 
 		// if it is chrome, and so is the origin, let it through
 		if (contentLocation.schemeIs('chrome') &&
 			requestOrigin && requestOrigin.schemeIs('chrome')
 		) {
-			return this.ACCEPT;
+			return ACCEPT;
 		}
 
 		// Only block in content windows (this from AdBlock Plus)
-		var win=this.windowForNode(requestingNode);
-		if (!win) return this.ACCEPT;
+		var win=windowForNode(requestingNode);
+		if (!win) return ACCEPT;
 		var winType=win
 			.QueryInterface(Components.interfaces.nsIInterfaceRequestor)
 			.getInterface(Components.interfaces.nsIWebNavigation)
 			.QueryInterface(Components.interfaces.nsIDocShellTreeItem)
 			.itemType;
 		if (winType!=Components.interfaces.nsIDocShellTreeItem.typeContent) {
-			return this.ACCEPT;
+			return ACCEPT;
 		}
 
 		// Start checking for whether we should block!
-		var fields=new this.Fields(
+		var fields=new Fields(
 			contentType, contentLocation, requestOrigin, requestingNode
 		);
 		var monitorGroups=[];
@@ -437,9 +436,9 @@ var gKablPolicy={
 			group=new cloneObject(group);
 			monitorGroups.push(group);
 
-			if (this.evalGroup(group, fields)) {
+			if (evalGroup(group, fields)) {
 				score+=group.score;
-				flag=this.evalScore('cutoff', score, fields);
+				flag=evalScore('cutoff', score, fields);
 				if (flag) break;
 			} else {
 				group.score=0;
@@ -447,24 +446,23 @@ var gKablPolicy={
 		}
 
 		if (!flag) {
-			flag=this.evalScore('threshold', score, fields);
+			flag=evalScore('threshold', score, fields);
 		}
 
-		if (!flag) flag=this.ACCEPT;
+		if (!flag) flag=ACCEPT;
 
-		this.monitorAdd(fields, monitorGroups, score, flag);
+		monitorAdd(fields, monitorGroups, score, flag);
 		return flag;
 
 		} catch (e) {
 			dump('ERROR IN kabl:\n');
 			for (i in e) dump(i+'	'+e[i]+'\n');
-			return this.ACCEPT;
+			return ACCEPT;
 		}
 	},
 
-	shouldProcess:function(
-		contentType, contentLocation, requestOrigin, requestingNode, mimeType, extra
-	) {
-		return this.ACCEPT;
+	shouldProcess:function(contentType, contentLocation, requestOrigin,
+			requestingNode, mimeType, extra) {
+		return ACCEPT;
 	}
 };
